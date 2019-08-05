3ca947923f75e51a456adaaad92e6a4210ab8fad
Marsha <marshareed11860@gmail.com>
2019-08-05 09:23:42 -0500

    Accept the date from the URL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ApodComponent } from './apod/apod.component';
 
 const routes: Routes = [
-  //3. redirect an empty route to a given path
   { path: '', redirectTo: '/apod', pathMatch: 'full'},
-  // 2. Define the path that will load a given component
-  { path: 'apod', component: ApodComponent }
+  { path: 'apod', component: ApodComponent },
+  { path: 'apod/:date', component: ApodComponent }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
